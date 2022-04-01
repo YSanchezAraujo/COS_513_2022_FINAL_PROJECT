6ba3d5318a96d5907e1066e2ae1feb97d7afc1c1
Yoel Sanchez Araujo <yoelsanchez006@gmail.com>
2022-04-01 18:48:24 -0400

    Update hmm_mle.py

--- src/python/hmm_mle.py
+++ src/python/hmm_mle.py
@@ -1,19 +1,60 @@
 import hmm
 
-def fit_hmm_em(y, likelihood_dists, tol, max_iter):
-    N, K = np.shape(data, 1) len(likelihood_dists)
+def fit_hmm_em(y, 
+              likelihood_dists,
+              tol=1e-5, max_iter=50):
+
+    N, K = np.shape(y)[0], len(likelihood_dists)
+
+    # initialize with k-means
+    init_params = init_kmeans(y, K)
+
     # initialize parameters: pi, Phi, mu, sig2
-    data_obj = data(np.zeros((N, K)), likelihood_dists, y)
-    # need to get initial parameters
-    
+    data = Data(np.zeros((N, K)), likelihood_dists, y, init_params)
+
     # initialize likelihood matrix
-    data_obj.compute_likelihoods()
+    data.compute_likelihoods()
+
+    # Q: do we actually need k-means initializations?
+    forw_mesg = Forward_message(
+        np.zeros((N, K)), _uniform_prob(K), 
+        np.zeros(N), np.random.random((K, K))
+    )
+
+    back_mesg = Backward_message(np.zeros((N, K)))
+    log_lik = 1e8
+
     for m in range(max_iter):
         # run forward message
+        forw_mesg.run(data)
+
         # run backward message
+        back_mesg.run(data, forw_mesg.Phi)
+
         # compute posteriors
+        gamma, mu, sig, pi, zeta = posterior_estimates(forw_mesg, back_mesg, data)
+
         # compute log-likelihood
-        # compute log-likelihood change
+        log_lik_m = np.sum(np.log(forw_mesg.Z))
+        log_lik_ch = np.abs(log_lik - log_lik_m)
+        log_lik = log_lik_m
+
         # update likelihood distributon parameters
+        likelihood_params = [(mu[k], sig[k]) for k in range(K)]
+        data.update_likelihood_params(likelihood_params)
+        data.compute_likelihoods()
+
+        # update forward object Phi
+        forw_mesg.Phi = zeta
+        forw_mesg.pi = pi
+
         # check convergence
+        if log_lik_ch < tol:
+            break
+
+    # create final object to regurn
+    posterior = {"mean": mu, "stdev": sig, "pi":pi, "zeta": zeta, "gamma": gamma}
+
+    return posterior, forw_mesg, back_mesg, data
+
     
