1127b8d0eed4553bfa343f5c6f77daf59a045d66
Yoel Sanchez Araujo <yoelsanchez006@gmail.com>
2022-04-01 18:48:03 -0400

    Update hmm.py

--- src/python/hmm.py
+++ src/python/hmm.py
@@ -1,32 +1,28 @@
-import numpy as np
-import scipy.stats as ss
-
-def _normalize_z(x):
-    Z = np.sum(x)
-    return x / Z, Z
+import utils
 
 # tested, this works
-class data:
-    def __init__(self, lik, psi, y):
+class Data:
+    def __init__(self, lik, psi, y, params):
         self.lik = lik
         self.psi = psi
         self.y = y
+        self.params = params
 
     def update_likelihood_params(self, params):
         # figure out a way to update this without naming
         # i.e. rely on input ordre
         for k in range(len(self.psi)):
-            self.psi[k] = self.psi[k](*params[k])
+            self.params[k] = params[k]
 
-    def compute_likelihods(self):
+    def compute_likelihoods(self):
         N = np.shape(self.y)[0]
         K = len(self.psi)
 
         for t in range(N):
             for k in range(K):
-                self.lik[t, k] = self.psi[k].pdf(self.y[t])
+                self.lik[t, k] = self.psi[k](*self.params[k]).pdf(self.y[t])
 
-class forward_message:
+class Forward_message:
     def __init__(self, alpha, pi, Z, Phi):
         """
             alpha: sufficient statistics estimated from e step
@@ -51,24 +47,22 @@ class forward_message:
             self.Z[t] = Z_t
         
 
-class backward_message:
-    def __init__(self, beta, Phi):
+class Backward_message:
+    def __init__(self, beta):
         """
             beta: sufficient statistics
-            Phi: Transition matrix
         """
         self.beta = beta
-        self.Phi = Phi
     
-    def run(self, data):
-        N = np.shape(data.lik)[1]
-        self.beta[N, :] = 1
+    def run(self, data, Phi):
+        N = np.shape(data.lik)[0]
+        self.beta[N-1, :] = 1
 
         for t in range(N-1, 0, -1):
-            beta_t, _ = _normalize_z(self.Phi @ (data.lik[t, :] * self.beta[t, :]))
+            beta_t, _ = _normalize_z(Phi @ (data.lik[t, :] * self.beta[t, :]))
             self.beta[t-1, :] = beta_t
 
-
+# problem areas to solve are here
 def posterior_estimates(forward_message, backward_message, data):
     N = np.shape(data.lik)[0]
     K = len(data.psi)
@@ -77,38 +71,25 @@ def posterior_estimates(forward_message, backward_message, data):
 
     for k in range(K):
         g = gamma[:, k]
-        mu[k] = g[:, np.newaxis].T @ data.y[:, np.newaxis] / np.sum(g)
+        mu[k] = (g[:, np.newaxis].T @ data.y[:, np.newaxis]) / np.sum(g)
     
     sig2, zeta = np.zeros(K), np.zeros((K, K, N))
-    y_no_mu = y - mu[:, np.newaxis]
+    y_no_mu = data.y[:, np.newaxis] - mu
 
     for k in range(K):
         g = gamma[:, k]
-        sig2[k] = g[:, np.newaxis].T @ y_no_mu**2 / np.sum(gamma[:, k])
+        sig2[k] = (g[:, np.newaxis].T @ y_no_mu[:, k]**2 )/ np.sum(gamma[:, k])
 
     pi = gamma[0, :] / np.sum(gamma[0, :])
 
-    for t in range(N):
+    for t in range(N-1):
         lik_beta = data.lik[t+1, :] * backward_message.beta[t+1, :]
         alpha = forward_message.alpha[t, :]
         zeta[:, :, t] = (
-            backward_message.Phi * (alpha[:, np.newaxis] @ lik_beta[:, np.newaxis].T)
+            forward_message.Phi * (alpha[:, np.newaxis] @ lik_beta[:, np.newaxis].T)
         ) / forward_message.Z[t+1]
 
-    zeta_nt = np.sum(zeta, axis=(2, 1))
-    zeta = np.sum(zeta, axis=2) / zeta_nt
+    #zeta_nt = np.sum(zeta, axis=(2, 1))
+    #zeta = np.sum(zeta, axis=2) / zeta_nt
 
-    return gamma, mu, np.sqrt(sig2), pi, zeta
-
-
-# testing
-N  = np.shape(dobj.lik)[0]
-K = len(dobj.psi)
-gamma = f.alpha * b.beta
-mu = np.zeros(K)
-
-for k in range(K):
-    g = gamma[:, k]
-    mu[k] = g[:, np.newaxis].T @ dobj.y / np.sum(g)
-
-sig2, zeta = np.zeros(K), np.zeros((K, K, N))
+    return gamma, mu, np.sqrt(sig2), pi, zeta.sum(2) / zeta.sum(2).sum(1)
