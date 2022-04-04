372af96a53b1be8f15a14ae46d7bc2add519b56f
Yoel Sanchez Araujo <yoelsanchez006@gmail.com>
2022-04-04 17:19:04 -0400

    adding gaussian hmm glm

---
+++ src/python/hmm_glm.py
@@ -0,0 +1,95 @@
+import utils
+
+# tested, this works
+class Data:
+    def __init__(self, lik, psi, y, stdevs, X, W):
+        self.lik = lik
+        self.psi = psi
+        self.y = y
+        self.stdevs = stdevs
+        self.X = X
+        self.W = W
+
+    def compute_likelihoods(self):
+        N = np.shape(self.y)[0]
+        K = len(self.psi)
+        
+        mus = np.mean(self.X @ self.W, axis=0)
+        for k in range(K):
+            self.lik[:, k] = self.psi[k](mus[k], self.stdevs[k]).pdf(self.y)
+
+class Forward_message:
+    def __init__(self, alpha, pi, Z, Phi):
+        """
+            alpha: sufficient statistics estimated from e step
+            pi: initial distribution
+            Z: normalizing constant
+            Phi: transition matrix
+        """ 
+        self.alpha = alpha
+        self.pi = pi
+        self.Z = Z
+        self.Phi = Phi
+
+    def run(self, data):
+        N = np.shape(data.lik)[0]
+        alpha_init, Z_init = _normalize_z(data.lik[0, :] * self.pi)
+        self.alpha[0, :] = alpha_init
+        self.Z[0] = Z_init
+
+        for t in range(1, N):
+            alpha_t, Z_t = _normalize_z(data.lik[t, :][:, np.newaxis] * 
+                                        (self.Phi.T @ self.alpha[t-1, :][:, np.newaxis]))
+            self.alpha[t, :] = alpha_t.ravel()
+            self.Z[t] = Z_t
+        
+
+class Backward_message:
+    def __init__(self, beta):
+        """
+            beta: sufficient statistics
+        """
+        self.beta = beta
+    
+    def run(self, data, Phi):
+        N = np.shape(data.lik)[0]
+        self.beta[N-1, :] = 1
+
+        for t in range(N-1, 0, -1):
+            beta_t, _ = _normalize_z(Phi @ (data.lik[t, :] * self.beta[t, :])[:, np.newaxis])
+            self.beta[t-1, :] = beta_t.ravel()
+
+# problem areas to solve are here
+def posterior_estimates(forward_message, backward_message, data):
+    N = np.shape(data.lik)[0]
+    K = len(data.psi)
+    gamma = forward_message.alpha * backward_message.beta
+    W = np.zeros((np.shape(data.X)[1], K))
+    X_proj = np.linalg.pinv(data.X.T @ data.X) @ data.X.T
+    p_z = gamma / gamma.sum(0)[np.newaxis, :]
+
+    for k in range(K):
+        W[:, k] = X_proj * p_z[:, k].T @ data.y
+
+    mu = np.mean(data.X @ W, axis=0)
+    # have to figure these out
+    sig2, zeta = np.zeros(K), np.zeros((N, K, K))
+    y_no_mu = data.y[:, np.newaxis] - mu
+    # remember these are clearly still wrong
+    for k in range(K):
+        g = gamma[:, k]
+        sig2[k] = (g[:, np.newaxis].T @ y_no_mu[:, k]**2 )/ np.sum(gamma[:, k])
+
+    pi = gamma[0, :] / np.sum(gamma[0, :])
+
+    for t in range(N-1):
+        lik_beta = data.lik[t+1, :] * backward_message.beta[t+1, :]
+        alpha = forward_message.alpha[t, :]
+        zeta[t, :, :] = (
+            forward_message.Phi * (alpha[:, np.newaxis] @ lik_beta[:, np.newaxis].T)
+        ) / forward_message.Z[t+1]
+
+    Phi = np.sum(zeta, axis=0)
+    Phi = Phi / Phi.sum(axis=1)[:, np.newaxis]
+
+    return gamma, np.sqrt(sig2), pi, Phi, W
